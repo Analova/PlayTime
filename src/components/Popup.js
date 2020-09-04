7a937c4fa6dc2d40088bf36474c66febbe28436b
Ana Nikolova <ani.nik19@gmail.com>
2020-09-04 06:50:38 +0200

    done

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { checkWin } from "../helpers/helpers";
 
-const Popup = ({ correctLetters, wrongLetters, selectedWord, setPlayable }) => {
+const Popup = ({
+  correctLetters,
+  wrongLetters,
+  selectedWord,
+  setPlayable,
+  playAgain,
+}) => {
   let finalMessage = "";
   let finalMessageRevealedWord = "";
-  let playbale = true;
+  let playable = true;
+
+  if (checkWin(correctLetters, wrongLetters, selectedWord) === "win") {
+    finalMessage = "Congratulations! You won! 😃";
+    playable = false;
+  } else if (checkWin(correctLetters, wrongLetters, selectedWord) === "lose") {
+    finalMessage = "Unfortunately you lost. 😕";
+    finalMessageRevealedWord = `...the word was: ${selectedWord}`;
+    playable = false;
+  }
+
+  useEffect(() => setPlayable(playable));
+
   return (
-    <div className="popup-container">
+    <div
+      className="popup-container"
+      style={finalMessage !== "" ? { display: "flex" } : {}}
+    >
       <div className="popup">
         <h2>{finalMessage}</h2>
         <h3>{finalMessageRevealedWord}</h3>
-        <button>Play Again</button>
+        <button onClick={playAgain}>Play Again</button>
       </div>
     </div>
   );
